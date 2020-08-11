ba9d3dbc419e388748cfc55bef3a33a37bee4b61
Zino-chan <webdevzino@gmail.com>
2020-08-11 11:22:17 +0100

    Added media queries for small devices changed glide js to splide js due to unfixed bug in glide js

--- src/js/main.js
+++ src/js/main.js
@@ -18,6 +18,160 @@ const pageTransition = () => {
     tl.set('.loader', {left: "-100%"});
 }
 
+const homeAnimation = () => {
+
+	gsap.from('.header', {
+		y: -100,
+		duration: 1,
+		opacity: 0,
+	})
+
+	gsap.from('.home-text h1',{
+		y: 100,
+		duration: 2,
+		opacity: 0,
+		delay: .5,
+		ease: "Expo.easeInOut"
+	})
+	gsap.from('.home-text p',{
+		y: -100,
+		duration: 2,
+		opacity: 0,
+		delay: 1,
+		ease: "Expo.easeInOut"
+	})
+	gsap.from('.home-text .main-btn ',{
+		x: -100,
+		duration: 2,
+		opacity: 0,
+		delay: 1.2,
+		ease: "Expo.easeInOut"
+	})
+	gsap.from('.home-text .btn-outline',{
+		x: 100,
+		duration: 2,
+		opacity: 0,
+		delay: 1.2,
+		ease: "Expo.easeInOut"
+	})
+}
+
+const aboutAnimation = () => {
+	gsap.from('.title h2', {
+		y: -100,
+		duration: 2,
+		opacity: 0,
+		delay: .5,
+		ease: "Expo.easeInOut"
+	})
+
+	gsap.from('.title p', {
+		y: 100,
+		duration: 2,
+		opacity: 0,
+		delay: 1,
+		ease: "Expo.easeInOut"
+	})
+
+	gsap.from('.about-content .about-img', {
+		scale: 0,
+		duration: 1.5,
+		opacity: 0,
+		delay: 1.3,
+		ease: "Expo.easeInOut"
+	})
+
+	gsap.from('.about-content h3', {
+		y: 100,
+		duration: 2,
+		opacity: 0,
+		delay: 1.5,
+		ease: "Expo.easeInOut"
+	})
+
+	gsap.from('.about-content p', {
+		y: 100,
+		duration: 2,
+		opacity: 0,
+		delay: 1.7,
+		ease: "Expo.easeInOut"
+	})
+
+}
+
+const serviceAnimation = () => {
+		gsap.from('.s-01-img', {
+			x: '-100%',
+			opacity: 0,
+			duration: 2,
+			ease: "Expo.easeInOut"
+		})
+
+		gsap.from('.s-01-content h2', {
+			y: 100,
+			opacity: 0,
+			duration: 1.5,
+			ease: "Expo.easeInOut"
+		})
+		gsap.from('.s-01-content .s-01-icon', {
+			scale: 0,
+			opacity: 0,
+			duration: 1.5,
+			stagger: 1.5,
+			delay: .5,
+			ease: "Expo.easeInOut"
+
+		})
+
+		gsap.from('.s-01-content h3', {
+			x: 100,
+			opacity: 0,
+			duration: 1.5,
+			delay: .2,
+			stagger: 1.5,
+			ease: "Expo.easeInOut"
+		})
+
+		gsap.from('.s-01-content p', {
+			y: 100,
+			opacity: 0,
+			duration: 1.5,
+			delay: .5,
+			stagger: 1.5,
+			ease: "Expo.easeInOut"
+		})
+}
+
+const pricingAnimation = () => {
+	gsap.from('.slide-left', {
+		x: 100,
+		opacity: 0,
+		duration: 2,
+		ease: "Expo.easeInOut",
+		delay: .5
+	})
+
+	gsap.from('.slide-right', {
+		x: -100,
+		opacity: 0,
+		duration: 2,
+		ease: "Expo.easeInOut",
+		delay: .5
+	})
+	gsap.from('.slide-in', {
+		scale: 0,
+		opacity: 0,
+		duration: 2,
+		ease: "Expo.easeInOut",
+		delay: 1.5
+	})
+}
+
+
+const glideSlider = () => {
+	return new Splide('.splide', {arrows: false}).mount()
+}
+
 
 barba.init({
 	 	sync: true,
@@ -28,10 +182,41 @@ barba.init({
 	 				pageTransition();
 	 				await delay(1000);
 	 				//data.current.container.remove();
-	 			},
-
+				},
+				async beforeEnter(data) {
+					document.querySelector('.header .navbar-nav').classList.remove('open');
+				}
 	 		},
-	 	],
+		 ],
+		views: [
+			{
+				namespace: 'home',
+				afterEnter(data) {	
+					homeAnimation();
+				},
+			},
+			{
+				namespace: 'about',
+				afterEnter(data) {	
+					aboutAnimation();
+				},
+			},
+			{
+				namespace: 'service',
+				beforeEnter(data) {	
+					serviceAnimation();
+					glideSlider();
+				},
+				
+			},
+			{
+				namespace: 'pricing',
+				afterEnter(data) {	
+					pricingAnimation();
+				},
+			},
+			  
+		]
 	 });
 
 
@@ -45,8 +230,6 @@ barba.init({
 	}
 
 
-new Glide('.glide').mount({
-	type: 'carousel',
-	startAt: 0,
-	perView: 1
+document.querySelector('.navbar-toggler').addEventListener('click', () => {
+	document.querySelector('.header .navbar-nav').classList.toggle('open');
 })
